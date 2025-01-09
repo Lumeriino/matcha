66d5836ae2cb8b5e23aa79e886254928b7009c4a
Lumeriino <merinomansoluciaadobe@gmail.com>
2025-01-09 17:41:26 +0100

    form

--- js/script_login.js
+++ js/script_login.js
@@ -4,10 +4,23 @@ const scroll = new LocomotiveScroll({
 });
 
 // Inicio
+
 document.getElementById("loginForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita que el formulario se envíe de manera predeterminada.
-    alert("Login submitted!"); // Muestra un mensaje de confirmación.
-    
+    event.preventDefault(); // Evita el envío predeterminado del formulario.
+
+    // Obtén los valores de los campos del formulario.
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    // Verifica si alguno de los campos está vacío.
+    if (!username || !password) {
+        alert("Complete the form!");
+        return; // Detiene la ejecución si los campos no están completos.
+    }
+
+    // Si los campos están completos, muestra el mensaje de confirmación.
+    alert("Login submitted!");
+
     // Redirige al usuario al home.
     window.location.href = "indexhome.html";
 });
